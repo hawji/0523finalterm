d87bec20ef3ea16a3face1dd8a82aec147fe10ef
jihyeon <zxs10341@gmail.com>
2024-06-11 02:11:04 +0900

    hi

--- script.js
+++ script.js
@@ -141,3 +141,55 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 });
 
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const section = document.querySelector('.backgorund'); // backgorund 클래스를 가진 요소를 선택합니다.
+    let lastStampTime = 0; // 마지막 도장 이미지가 찍힌 시간을 저장합니다.
+    const minStampInterval = 500; // 최소 도장 이미지 간격 (밀리초 단위)
+    const stampSize = 500; // 도장 이미지의 크기입니다.
+
+    section.addEventListener('mouseenter', () => {
+        // 커서를 숨깁니다.
+        section.classList.add('no-cursor');
+    });
+
+    section.addEventListener('mouseleave', () => {
+        // 커서를 다시 보여줍니다.
+        section.classList.remove('no-cursor');
+    });
+
+    section.addEventListener('mousemove', (e) => {
+        // 현재 시간을 가져옵니다.
+        const currentTime = new Date().getTime();
+
+        // 이전 도장 이미지와의 간격을 확인합니다.
+        if (currentTime - lastStampTime >= minStampInterval) {
+            // 도장 효과를 생성합니다.
+            const stampImage = document.createElement('img');
+            stampImage.src = 'image/Group%2048.png'; // 도장 이미지 경로를 지정합니다.
+            stampImage.className = 'stamp-image';
+            stampImage.style.position = 'absolute';
+            stampImage.style.width = `${stampSize}px`; // 도장 이미지의 크기를 조정합니다.
+            stampImage.style.height = `${stampSize}px`; // 도장 이미지의 크기를 조정합니다.
+            stampImage.style.left = `${e.clientX - stampSize / 2}px`; // 도장 이미지를 마우스 위치 중심에 위치시킵니다.
+            stampImage.style.top = `${e.clientY - stampSize / 2}px`; // 도장 이미지를 마우스 위치 중심에 위치시킵니다.
+
+            section.appendChild(stampImage); // 도장 이미지를 섹션에 추가합니다.
+
+            // 마지막 도장 이미지가 찍힌 시간을 업데이트합니다.
+            lastStampTime = currentTime;
+
+            // 일정 시간 후에 도장 이미지를 제거합니다.
+            setTimeout(() => {
+                stampImage.remove();
+            }, 5000); // 도장이 찍힌 후 1초 후에 이미지를 제거합니다.
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const backgroundMusic = document.getElementById('background-music');
+    const volumeRatio = 0.20 // 음량을 줄일 비율 (1/3)
+
+    backgroundMusic.volume = volumeRatio;
+});
